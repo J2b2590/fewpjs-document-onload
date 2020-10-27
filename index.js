10c5bd04486ea505e5faad45eafff301302d154d
J2b2590 <J2b2590@gmail.com>
2020-10-27 11:24:20 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded");
+    let text = document.getElementById("text");
+
+    text.innerHTML = "This is really cool!"
+})
+
